11ab773c036a1743e6588fe181ed6a84a6bf34b1
MarcosGalizz <marcgalizzi10@gmail.com>
2025-09-19 18:49:51 -0300

    Agregar archivos de proyecto.

---
+++ pryGalizziAgenda/frmActividades.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace pryGalizziAgenda
+{
+    partial class frmActividades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaLblActividad = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.gunaLblFecha = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.gunaLblObservacion = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.gunaBtnAceptar = new Guna.UI2.WinForms.Guna2Button();
+            this.gunaBtnCancelar = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2DateTimePicker1 = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // gunaLblActividad
+            // 
+            this.gunaLblActividad.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLblActividad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLblActividad.Location = new System.Drawing.Point(29, 40);
+            this.gunaLblActividad.Name = "gunaLblActividad";
+            this.gunaLblActividad.Size = new System.Drawing.Size(59, 17);
+            this.gunaLblActividad.TabIndex = 4;
+            this.gunaLblActividad.Text = "Actividad";
+            // 
+            // gunaLblFecha
+            // 
+            this.gunaLblFecha.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLblFecha.Location = new System.Drawing.Point(29, 99);
+            this.gunaLblFecha.Name = "gunaLblFecha";
+            this.gunaLblFecha.Size = new System.Drawing.Size(41, 17);
+            this.gunaLblFecha.TabIndex = 5;
+            this.gunaLblFecha.Text = "Fecha";
+            // 
+            // gunaLblObservacion
+            // 
+            this.gunaLblObservacion.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLblObservacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLblObservacion.Location = new System.Drawing.Point(29, 167);
+            this.gunaLblObservacion.Name = "gunaLblObservacion";
+            this.gunaLblObservacion.Size = new System.Drawing.Size(80, 17);
+            this.gunaLblObservacion.TabIndex = 6;
+            this.gunaLblObservacion.Text = "Observación";
+            // 
+            // gunaBtnAceptar
+            // 
+            this.gunaBtnAceptar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnAceptar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnAceptar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaBtnAceptar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaBtnAceptar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaBtnAceptar.ForeColor = System.Drawing.Color.White;
+            this.gunaBtnAceptar.Location = new System.Drawing.Point(135, 306);
+            this.gunaBtnAceptar.Name = "gunaBtnAceptar";
+            this.gunaBtnAceptar.Size = new System.Drawing.Size(180, 45);
+            this.gunaBtnAceptar.TabIndex = 7;
+            this.gunaBtnAceptar.Text = "Aceptar";
+            // 
+            // gunaBtnCancelar
+            // 
+            this.gunaBtnCancelar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnCancelar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnCancelar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaBtnCancelar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaBtnCancelar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaBtnCancelar.ForeColor = System.Drawing.Color.White;
+            this.gunaBtnCancelar.Location = new System.Drawing.Point(389, 306);
+            this.gunaBtnCancelar.Name = "gunaBtnCancelar";
+            this.gunaBtnCancelar.Size = new System.Drawing.Size(180, 45);
+            this.gunaBtnCancelar.TabIndex = 8;
+            this.gunaBtnCancelar.Text = "Cancelar";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(125, 30);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(468, 39);
+            this.guna2TextBox1.TabIndex = 9;
+            // 
+            // guna2DateTimePicker1
+            // 
+            this.guna2DateTimePicker1.Checked = true;
+            this.guna2DateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.guna2DateTimePicker1.Location = new System.Drawing.Point(125, 90);
+            this.guna2DateTimePicker1.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.guna2DateTimePicker1.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.guna2DateTimePicker1.Name = "guna2DateTimePicker1";
+            this.guna2DateTimePicker1.Size = new System.Drawing.Size(261, 36);
+            this.guna2DateTimePicker1.TabIndex = 10;
+            this.guna2DateTimePicker1.Value = new System.DateTime(2025, 9, 19, 18, 47, 41, 117);
+            // 
+            // guna2TextBox2
+            // 
+            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox2.DefaultText = "";
+            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox2.Location = new System.Drawing.Point(125, 157);
+            this.guna2TextBox2.Multiline = true;
+            this.guna2TextBox2.Name = "guna2TextBox2";
+            this.guna2TextBox2.PlaceholderText = "";
+            this.guna2TextBox2.SelectedText = "";
+            this.guna2TextBox2.Size = new System.Drawing.Size(458, 123);
+            this.guna2TextBox2.TabIndex = 11;
+            // 
+            // frmActividades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 387);
+            this.Controls.Add(this.guna2TextBox2);
+            this.Controls.Add(this.guna2DateTimePicker1);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.gunaBtnCancelar);
+            this.Controls.Add(this.gunaBtnAceptar);
+            this.Controls.Add(this.gunaLblObservacion);
+            this.Controls.Add(this.gunaLblFecha);
+            this.Controls.Add(this.gunaLblActividad);
+            this.Name = "frmActividades";
+            this.Text = "Actividades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel gunaLblActividad;
+        private Guna.UI2.WinForms.Guna2HtmlLabel gunaLblFecha;
+        private Guna.UI2.WinForms.Guna2HtmlLabel gunaLblObservacion;
+        private Guna.UI2.WinForms.Guna2Button gunaBtnAceptar;
+        private Guna.UI2.WinForms.Guna2Button gunaBtnCancelar;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+    }
+}
---
+++ pryGalizziAgenda/frmInicio.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace pryGalizziAgenda
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gunaBtnAgregar = new Guna.UI2.WinForms.Guna2Button();
+            this.gunaBtnEliminar = new Guna.UI2.WinForms.Guna2Button();
+            this.gunaBtnSalir = new Guna.UI2.WinForms.Guna2Button();
+            this.gunaLblTituloAgenda = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.gunaDgvAgenda = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.asunto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.observacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDgvAgenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gunaBtnAgregar
+            // 
+            this.gunaBtnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaBtnAgregar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnAgregar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnAgregar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaBtnAgregar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaBtnAgregar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaBtnAgregar.ForeColor = System.Drawing.Color.White;
+            this.gunaBtnAgregar.Location = new System.Drawing.Point(49, 368);
+            this.gunaBtnAgregar.Name = "gunaBtnAgregar";
+            this.gunaBtnAgregar.Size = new System.Drawing.Size(180, 45);
+            this.gunaBtnAgregar.TabIndex = 0;
+            this.gunaBtnAgregar.Text = "Agregar actividad";
+            this.gunaBtnAgregar.Click += new System.EventHandler(this.gunaBtnAgregar_Click);
+            // 
+            // gunaBtnEliminar
+            // 
+            this.gunaBtnEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaBtnEliminar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnEliminar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnEliminar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaBtnEliminar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaBtnEliminar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaBtnEliminar.ForeColor = System.Drawing.Color.White;
+            this.gunaBtnEliminar.Location = new System.Drawing.Point(283, 368);
+            this.gunaBtnEliminar.Name = "gunaBtnEliminar";
+            this.gunaBtnEliminar.Size = new System.Drawing.Size(180, 45);
+            this.gunaBtnEliminar.TabIndex = 1;
+            this.gunaBtnEliminar.Text = "Eliminar actividad";
+            // 
+            // gunaBtnSalir
+            // 
+            this.gunaBtnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaBtnSalir.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnSalir.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaBtnSalir.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaBtnSalir.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaBtnSalir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaBtnSalir.ForeColor = System.Drawing.Color.White;
+            this.gunaBtnSalir.Location = new System.Drawing.Point(514, 368);
+            this.gunaBtnSalir.Name = "gunaBtnSalir";
+            this.gunaBtnSalir.Size = new System.Drawing.Size(180, 45);
+            this.gunaBtnSalir.TabIndex = 2;
+            this.gunaBtnSalir.Text = "Salir";
+            // 
+            // gunaLblTituloAgenda
+            // 
+            this.gunaLblTituloAgenda.AutoSize = false;
+            this.gunaLblTituloAgenda.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLblTituloAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLblTituloAgenda.Location = new System.Drawing.Point(252, 22);
+            this.gunaLblTituloAgenda.Name = "gunaLblTituloAgenda";
+            this.gunaLblTituloAgenda.Size = new System.Drawing.Size(225, 31);
+            this.gunaLblTituloAgenda.TabIndex = 3;
+            this.gunaLblTituloAgenda.Text = "Agenda de actividades";
+            // 
+            // gunaDgvAgenda
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.gunaDgvAgenda.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gunaDgvAgenda.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gunaDgvAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gunaDgvAgenda.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.asunto,
+            this.fecha,
+            this.observacion});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gunaDgvAgenda.DefaultCellStyle = dataGridViewCellStyle3;
+            this.gunaDgvAgenda.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDgvAgenda.Location = new System.Drawing.Point(49, 59);
+            this.gunaDgvAgenda.Name = "gunaDgvAgenda";
+            this.gunaDgvAgenda.RowHeadersVisible = false;
+            this.gunaDgvAgenda.Size = new System.Drawing.Size(645, 265);
+            this.gunaDgvAgenda.TabIndex = 4;
+            this.gunaDgvAgenda.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDgvAgenda.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.gunaDgvAgenda.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.gunaDgvAgenda.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.gunaDgvAgenda.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.gunaDgvAgenda.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDgvAgenda.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gunaDgvAgenda.ThemeStyle.HeaderStyle.Height = 15;
+            this.gunaDgvAgenda.ThemeStyle.ReadOnly = false;
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.Height = 22;
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDgvAgenda.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // asunto
+            // 
+            this.asunto.HeaderText = "Asunto";
+            this.asunto.Name = "asunto";
+            // 
+            // fecha
+            // 
+            this.fecha.HeaderText = "Fecha";
+            this.fecha.Name = "fecha";
+            // 
+            // observacion
+            // 
+            this.observacion.HeaderText = "Observacion";
+            this.observacion.Name = "observacion";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gunaDgvAgenda);
+            this.Controls.Add(this.gunaLblTituloAgenda);
+            this.Controls.Add(this.gunaBtnSalir);
+            this.Controls.Add(this.gunaBtnEliminar);
+            this.Controls.Add(this.gunaBtnAgregar);
+            this.Name = "frmInicio";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.frmInicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDgvAgenda)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button gunaBtnAgregar;
+        private Guna.UI2.WinForms.Guna2Button gunaBtnEliminar;
+        private Guna.UI2.WinForms.Guna2Button gunaBtnSalir;
+        private Guna.UI2.WinForms.Guna2HtmlLabel gunaLblTituloAgenda;
+        private Guna.UI2.WinForms.Guna2DataGridView gunaDgvAgenda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn asunto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn observacion;
+    }
+}
